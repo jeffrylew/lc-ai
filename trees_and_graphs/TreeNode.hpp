9e5d6844137167fd586b26da24bdc5a2def93fb1
Jeffry Lew <jeffryklew@gmail.com>
2025-07-22 23:40:29 -0400

    Implement trees_and_graphs/TreeNode.hpp

---
+++ trees_and_graphs/TreeNode.hpp
@@ -0,0 +1,20 @@
+struct TreeNode
+{
+    int       val {};
+    TreeNode* left {nullptr};
+    TreeNode* right {nullptr};
+
+    TreeNode() = default;
+
+    constexpr explicit TreeNode(int x)
+        : val {x}
+    {
+    }
+
+    explicit TreeNode(int x, TreeNode* left_in, TreeNode* right_in)
+        : val {x}
+        , left {left_in}
+        , right {right_in}
+    {
+    }
+};
