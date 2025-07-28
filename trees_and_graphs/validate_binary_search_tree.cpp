78793fe47acb4a7ca89d7f2d1c2720261e07758a
Jeffry Lew <jeffryklew@gmail.com>
2025-07-28 17:32:28 -0400

    Implement iterative traversal with valid range discussion solution in trees_and_graphs/validate_binary_search_tree.cpp

--- trees_and_graphs/validate_binary_search_tree.cpp
+++ trees_and_graphs/validate_binary_search_tree.cpp
@@ -2,6 +2,8 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <stack>
+
 //! @brief First attempt solution to determine if a binary tree is a valid BST
 //! @param[in] root Pointer to root TreeNode
 //! @return True if binary tree is a valid binary search tree, else false
@@ -76,6 +78,62 @@ static bool isValidBSTDS1(TreeNode* root)
     return validate_bst(root, nullptr, nullptr);
 }
 
+//! @struct BoundedTreeNode
+//! @brief Pointer to TreeNode and the low and high bounding TreeNodes
+struct BoundedTreeNode
+{
+    TreeNode* node {nullptr};
+    TreeNode* low_node {nullptr};
+    TreeNode* high_node {nullptr};
+};
+
+//! @brief Iterative traversal with valid range discussion solution
+//! @param[in] root Pointer to root TreeNode
+//! @return True if binary tree is a valid binary search tree, else false
+static bool isValidBSTDS2(TreeNode* root)
+{
+    //! @details https://leetcode.com/problems/validate-binary-search-tree/
+    //!
+    //!          Time complexity O(N) to visit each node exactly once
+    //!          Space complexity O(N) to store up to the entire tree
+
+    std::stack<BoundedTreeNode> bounded_node_stack;
+    bounded_node_stack.emplace(root, nullptr, nullptr);
+
+    while (!bounded_node_stack.empty())
+    {
+        auto bounded_node = bounded_node_stack.top();
+        bounded_node_stack.pop();
+
+        if (bounded_node.node == nullptr)
+        {
+            continue;
+        }
+
+        if (bounded_node.low_node != nullptr
+            && bounded_node.node->val <= bounded_node.low_node->val)
+        {
+            return false;
+        }
+
+        if (bounded_node.high_node != nullptr
+            && bounded_node.node->val >= bounded_node.high_node->val)
+        {
+            return false;
+        }
+
+        bounded_node_stack.emplace(bounded_node.node->right,
+                                   bounded_node.node,
+                                   bounded_node.high_node);
+        bounded_node_stack.emplace(bounded_node.node->left,
+                                   bounded_node.low_node,
+                                   bounded_node.node);
+    }
+
+    return true;
+
+} // static bool isValidBSTDS2( ...
+
 TEST_CASE("Example 1", "[isValidBST]")
 {
     TreeNode one {1};
@@ -84,6 +142,7 @@ TEST_CASE("Example 1", "[isValidBST]")
 
     REQUIRE(isValidBSTFA(&two));
     REQUIRE(isValidBSTDS1(&two));
+    REQUIRE(isValidBSTDS2(&two));
 }
 
 TEST_CASE("Example 2", "[isValidBST]")
@@ -97,6 +156,7 @@ TEST_CASE("Example 2", "[isValidBST]")
 
     REQUIRE(!isValidBSTFA(&five));
     REQUIRE(!isValidBSTDS1(&five));
+    REQUIRE(!isValidBSTDS2(&five));
 }
 
 TEST_CASE("Example 3", "[isValidBST]")
@@ -106,6 +166,7 @@ TEST_CASE("Example 3", "[isValidBST]")
 
     REQUIRE(isValidBSTFA(&zero));
     REQUIRE(isValidBSTDS1(&zero));
+    REQUIRE(isValidBSTDS2(&zero));
 }
 
 TEST_CASE("Example 4", "[isValidBST]")
@@ -119,4 +180,5 @@ TEST_CASE("Example 4", "[isValidBST]")
 
     // REQUIRE(!isValidBSTFA(&five));
     REQUIRE(!isValidBSTDS1(&five));
+    REQUIRE(!isValidBSTDS2(&five));
 }
