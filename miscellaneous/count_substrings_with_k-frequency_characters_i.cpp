187414296bd4cb58ac0bf768587055d17297c8c4
Jeffry Lew <jeffryklew@gmail.com>
2025-08-14 21:21:19 -0400

    Implement second discussion solution in count_substrings_with_k-frequency_characters_i.cpp

--- miscellaneous/count_substrings_with_k-frequency_characters_i.cpp
+++ miscellaneous/count_substrings_with_k-frequency_characters_i.cpp
@@ -44,6 +44,45 @@ static int numberOfSubstringsDS1(std::string s, int k)
     return num_substrings;
 }
 
+//! @brief Get total substrings of s where at least one char appears >= k times
+//! @param[in] s String to get substrings from
+//! @param[in] k At least one character in substring appears at least k times
+//! @return Total number of substrings
+static int numberOfSubstringsDS2(std::string s, int k)
+{
+    //! @details https://leetcode.com/problems
+    //!            /count-substrings-with-k-frequency-characters-i/description/
+    //!          https://leetcode.com/problems
+    //!            /count-substrings-with-k-frequency-characters-i/solutions
+    //!            /5939947/javacpython-sliding-window-on-by-lee215-91da/
+    //!
+    //!          Time complexity O(N) where N = s.size().
+    //!          Space complexity O(1).
+
+    const auto num_chars = static_cast<int>(std::ssize(s));
+
+    //! There are (N + 1) * N / 2 substrings in total
+    int num_substrings {(num_chars + 1) * num_chars / 2};
+
+    std::unordered_map<char, int> char_counts {};
+
+    int left_idx {};
+    for (int right_idx = 0; right_idx < num_substrings; ++right_idx)
+    {
+        ++char_counts[s[right_idx]];
+
+        while (char_counts[s[right_idx]] >= k)
+        {
+            --char_counts[s[left_idx]];
+            ++left_idx;
+        }
+
+        num_substrings -= (right_idx - left_idx + 1);
+    }
+
+    return num_substrings;
+}
+
 TEST_CASE("Example 1", "[numberOfSubstrings]")
 {
     //! Valid substrings are
@@ -52,10 +91,12 @@ TEST_CASE("Example 1", "[numberOfSubstrings]")
     //! - "abacb" ('a' appears 2 times)
     //! - "bacb" ('b' appears 2 times)
     REQUIRE(4 == numberOfSubstringsDS1("abacb", 2));
+    REQUIRE(4 == numberOfSubstringsDS2("abacb", 2));
 }
 
 TEST_CASE("Example 2", "[numberOfSubstrings]")
 {
     //! All substrings are valid because every character appears at least once
     REQUIRE(15 == numberOfSubstringsDS1("abcde", 1));
+    REQUIRE(15 == numberOfSubstringsDS2("abcde", 1));
 }
