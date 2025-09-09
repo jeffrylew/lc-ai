81006bab9aeba0162bbefd5ed15f32c46f148d09
Jeffry Lew <jeffryklew@gmail.com>
2025-09-09 19:05:04 -0400

    Implement topological sort using Kahn's Algorithm discussion solution in trees_and_graphs/course_schedule.cpp

--- trees_and_graphs/course_schedule.cpp
+++ trees_and_graphs/course_schedule.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -33,6 +34,87 @@ static bool canFinishFA(int                                  numCourses,
     return can_finish;
 }
 
+static bool canFinishDS1(int                                  numCourses,
+                         const std::vector<std::vector<int>>& prerequisites)
+{
+    //! @details https://leetcode.com/problems/course-schedule/editorial/
+    //!
+    //!          Time complexity O(M + N) where M = prerequisites.size() and
+    //!          N = number of courses. Initializing prereq_and_courses takes
+    //!          O(M) to go through all edges. Initializing course_indegrees
+    //!          also takes O(M). Each queue operation takes O(1) and a single
+    //!          course/node will be pushed once, leading to O(N) operations for
+    //!          N courses/nodes. We iterate over the neighbors of each course
+    //!          /node that is popped out of the queue iterating over all the
+    //!          edges once. Since there are a total of M edges, it would take
+    //!          O(M) to iterate over the edges.
+    //!          Space complexity O(M + N). course_indegrees and
+    //!          prereq_and_courses take O(M) space. course_queue can have no
+    //!          more than N elements in the worst-case scenario.
+
+    //! Map of <course, number of prereqs/edges entering course>
+    std::unordered_map<int, int> course_indegrees;
+
+    //! Map of <prerequisite, courses that have this prereq>
+    //! Vector contains all courses with an incoming edge from prerequisite
+    std::unordered_map<int, std::vector<int>> prereq_and_courses;
+
+    for (const auto& course_and_prereq : prerequisites)
+    {
+        const int course {course_and_prereq[0]};
+        const int prereq {course_and_prereq[1]};
+
+        prereq_and_courses[prereq].push_back(course);
+        ++course_indegrees[course];
+    }
+
+    //! Queue for BFS moving from leaf courses/nodes to parent courses/nodes
+    std::queue<int> course_queue;
+
+    //! Push all courses with indegree zero into the queue
+    for (int course = 0; course < numCourses; ++course)
+    {
+        if (course_indegrees.contains(course))
+        {
+            continue;
+        }
+
+        course_queue.push(course);
+    }
+
+    //! Count number of visited/taken courses
+    int taken_courses {};
+
+    while (!course_queue.empty())
+    {
+        const int course {course_queue.front()};
+        course_queue.pop();
+        ++taken_courses;
+
+        if (!prereq_and_courses.contains(course))
+        {
+            continue;
+        }
+
+        //! neighbor_course has an incoming edge from course
+        for (const auto& neighbor_course : prereq_and_courses[course])
+        {
+            //! Delete the edge "course -> neighbor_course"
+            --course_indegrees[neighbor_course];
+
+            if (course_indegrees[neighbor_course] == 0)
+            {
+                //! neighbor_course behaves as a leaf course/node
+                course_queue.push(neighbor_course);
+            }
+        }
+    }
+
+    //! If number of courses taken/visited is less than total number of courses
+    //! then return false as there must be a cycle.
+    return taken_courses == numCourses;
+}
+
 TEST_CASE("Example 1", "[canFinish]")
 {
     constexpr int numCourses {2};
@@ -40,6 +122,7 @@ TEST_CASE("Example 1", "[canFinish]")
     const std::vector<std::vector<int>> prerequisites {{1, 0}};
 
     // REQUIRE(canFinishFA(numCourses, prerequisites));
+    REQUIRE(canFinishDS1(numCourses, prerequisites));
 }
 
 TEST_CASE("Example 2", "[canFinish]")
@@ -49,4 +132,5 @@ TEST_CASE("Example 2", "[canFinish]")
     const std::vector<std::vector<int>> prerequisites {{1, 0}, {0, 1}};
 
     // REQUIRE(!canFinishFA(numCourses, prerequisites));
+    REQUIRE(!canFinishDS1(numCourses, prerequisites));
 }
