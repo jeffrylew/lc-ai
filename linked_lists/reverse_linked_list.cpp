f3e9976b2fbf28ea54ac46f654a1fec119208159
Jeffry Lew <jeffryklew@gmail.com>
2025-09-23 20:57:49 -0400

    Implement recursive discussion solution in linked_lists/reverse_linked_list.cpp

--- linked_lists/reverse_linked_list.cpp
+++ linked_lists/reverse_linked_list.cpp
@@ -24,6 +24,24 @@ static ListNode* reverseListFA(ListNode* head)
     return new_head;
 }
 
+//! DS1 is same as FA
+
+static ListNode* reverseListDS2(ListNode* head)
+{
+    //! @details https://leetcode.com/problems/reverse-linked-list/editorial/
+
+    if (head == nullptr || head->next == nullptr)
+    {
+        return head;
+    }
+
+    auto* new_head   = reverseListDS2(head->next);
+    head->next->next = head;
+    head->next       = nullptr;
+
+    return new_head;
+}
+
 TEST_CASE("Example 1", "[reverseList]")
 {
     ListNode five {5};
@@ -44,6 +62,21 @@ TEST_CASE("Example 1", "[reverseList]")
     REQUIRE(reversed_head_fa->next->next->next->val == 2);
     REQUIRE(reversed_head_fa->next->next->next->next != nullptr);
     REQUIRE(reversed_head_fa->next->next->next->next->val == 1);
+
+    /*
+    auto* reversed_head_ds2 = reverseListDS2(&one);
+
+    REQUIRE(reversed_head_ds2 != nullptr);
+    REQUIRE(reversed_head_ds2->val == 5);
+    REQUIRE(reversed_head_ds2->next != nullptr);
+    REQUIRE(reversed_head_ds2->next->val == 4);
+    REQUIRE(reversed_head_ds2->next->next != nullptr);
+    REQUIRE(reversed_head_ds2->next->next->val == 3);
+    REQUIRE(reversed_head_ds2->next->next->next != nullptr);
+    REQUIRE(reversed_head_ds2->next->next->next->val == 2);
+    REQUIRE(reversed_head_ds2->next->next->next->next != nullptr);
+    REQUIRE(reversed_head_ds2->next->next->next->next->val == 1);
+     */
 }
 
 TEST_CASE("Example 2", "[reverseList]")
@@ -57,9 +90,19 @@ TEST_CASE("Example 2", "[reverseList]")
     REQUIRE(reversed_head_fa->val == 2);
     REQUIRE(reversed_head_fa->next != nullptr);
     REQUIRE(reversed_head_fa->next->val == 1);
+
+    /*
+    auto* reversed_head_ds2 = reverseListDS2(&one);
+
+    REQUIRE(reversed_head_ds2 != nullptr);
+    REQUIRE(reversed_head_ds2->val == 2);
+    REQUIRE(reversed_head_ds2->next != nullptr);
+    REQUIRE(reversed_head_ds2->next->val == 1);
+     */
 }
 
 TEST_CASE("Example 3", "[reverseList]")
 {
     REQUIRE(nullptr == reverseListFA(nullptr));
+    REQUIRE(nullptr == reverseListDS2(nullptr));
 }
