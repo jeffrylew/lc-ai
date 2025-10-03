2f8ca74a5070c90ed3e2f803c16404161c24be2d
Jeffry Lew <jeffryklew@gmail.com>
2025-10-02 22:03:25 -0400

    Implement first attempt solution in arrays_and_strings/group_anagrams.cpp

--- arrays_and_strings/group_anagrams.cpp
+++ arrays_and_strings/group_anagrams.cpp
@@ -1,6 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
 #include <string>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 static std::vector<std::vector<std::string>>
@@ -8,8 +11,31 @@ static std::vector<std::vector<std::string>>
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/2970/
+    //!
+    //!          Time complexity O(N * S * log S) where N = strs.size() and
+    //!          S = the average length of a string in strs. We sort each string
+    //!          in O(S * log S) and repeat for all O(N) strings in strs.
+    //!          Space complexity O(N) to store all strings in strs.
 
-    
+    std::unordered_map<std::string, std::vector<std::string>> anagram_groups;
+
+    for (const auto& str : strs)
+    {
+        auto alphabetical_str = str;
+        std::ranges::sort(alphabetical_str);
+
+        anagram_groups[std::move(alphabetical_str)].push_back(str);
+    }
+
+    std::vector<std::vector<std::string>> grouped_anagrams;
+    grouped_anagrams.reserve(anagram_groups.size());
+
+    for (auto& [alphabetical_key, anagram_vec] : anagram_groups)
+    {
+        grouped_anagrams.push_back(std::move(anagram_vec));
+    }
+
+    return grouped_anagrams;
 }
 
 TEST_CASE("Example 1", "[groupAnagrams]")
