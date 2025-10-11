fe0501a1252dc44e70a619f01acfda0c134c3729
Jeffry Lew <jeffryklew@gmail.com>
2025-10-10 22:31:49 -0400

    Implement iterative discussion solution using parent pointers in lowest_common_ancestor_of_a_binary_tree.cpp

--- trees_and_graphs/lowest_common_ancestor_of_a_binary_tree.cpp
+++ trees_and_graphs/lowest_common_ancestor_of_a_binary_tree.cpp
@@ -3,6 +3,9 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <functional>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
 
 static TreeNode* lowestCommonAncestorDS1(TreeNode* root,
                                          TreeNode* p,
@@ -80,6 +83,61 @@ static TreeNode* lowestCommonAncestorDS2(TreeNode* root,
     return lca_node;
 }
 
+static TreeNode* lowestCommonAncestorDS3(TreeNode* root,
+                                         TreeNode* p,
+                                         TreeNode* q)
+{
+    //! @details https://leetcode.com/problems
+    //!          /lowest-common-ancestor-of-a-binary-tree/editorial/
+
+    //! Stack for tree traversal
+    std::stack<TreeNode*> node_stack;
+    node_stack.push(root);
+
+    //! Map of <pointer to child node, pointer to parent node>
+    std::unordered_map<TreeNode*, TreeNode*> child_to_parent_map;
+    child_to_parent_map[root] = nullptr;
+
+    //! Iterate until we find both nodes p and q
+    while (!child_to_parent_map.contains(p) || !child_to_parent_map.contains(q))
+    {
+        auto* curr_node = node_stack.top();
+        node_stack.pop();
+
+        //! While traversing the tree, save the pointers to parent nodes
+        if (curr_node->left != nullptr)
+        {
+            child_to_parent_map[curr_node->left] = curr_node;
+            node_stack.push(curr_node->left);
+        }
+
+        if (curr_node->right != nullptr)
+        {
+            child_to_parent_map[curr_node->right] = curr_node;
+            node_stack.push(curr_node->right);
+        }
+    }
+
+    //! Set of ancestors for node p
+    std::unordered_set<TreeNode*> p_ancestors;
+
+    //! Process all ancestors of node p
+    while (p != nullptr)
+    {
+        p_ancestors.insert(p);
+        p = child_to_parent_map[p];
+    }
+
+    //! The first ancestor of q that appears in p's ancestor set
+    //! is their lowest common ancestor
+    while (!p_ancestors.contains(q))
+    {
+        q = child_to_parent_map[q];
+    }
+
+    return q;
+}
+
 TEST_CASE("Example 1", "[lowestCommonAncestor]")
 {
     TreeNode zero {0};
@@ -101,6 +159,10 @@ TEST_CASE("Example 1", "[lowestCommonAncestor]")
     auto* lca_ds2 = lowestCommonAncestorDS2(&three, &five, &one);
     REQUIRE(lca_ds2 != nullptr);
     REQUIRE(lca_ds2->val == 3);
+
+    auto* lca_ds3 = lowestCommonAncestorDS3(&three, &five, &one);
+    REQUIRE(lca_ds3 != nullptr);
+    REQUIRE(lca_ds3->val == 3);
 }
 
 TEST_CASE("Example 2", "[lowestCommonAncestor]")
@@ -121,9 +183,13 @@ TEST_CASE("Example 2", "[lowestCommonAncestor]")
     REQUIRE(lca_ds1 != nullptr);
     REQUIRE(lca_ds1->val == 5);
 
-    auto* lca_ds2 = lowestCommonAncestorDS1(&three, &five, &four);
+    auto* lca_ds2 = lowestCommonAncestorDS2(&three, &five, &four);
     REQUIRE(lca_ds2 != nullptr);
     REQUIRE(lca_ds2->val == 5);
+
+    auto* lca_ds3 = lowestCommonAncestorDS3(&three, &five, &four);
+    REQUIRE(lca_ds3 != nullptr);
+    REQUIRE(lca_ds3->val == 5);
 }
 
 TEST_CASE("Example 3", "[lowestCommonAncestor]")
@@ -135,7 +201,11 @@ TEST_CASE("Example 3", "[lowestCommonAncestor]")
     REQUIRE(lca_ds1 != nullptr);
     REQUIRE(lca_ds1->val == 1);
 
-    auto* lca_ds2 = lowestCommonAncestorDS1(&one, &one, &two);
+    auto* lca_ds2 = lowestCommonAncestorDS2(&one, &one, &two);
     REQUIRE(lca_ds2 != nullptr);
     REQUIRE(lca_ds2->val == 1);
+
+    auto* lca_ds3 = lowestCommonAncestorDS3(&one, &one, &two);
+    REQUIRE(lca_ds3 != nullptr);
+    REQUIRE(lca_ds3->val == 1);
 }
