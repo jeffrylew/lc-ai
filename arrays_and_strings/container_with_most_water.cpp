4a4be5d70261a8f0d140fec935d4ed34ef5e55d2
Jeffry Lew <jeffryklew@gmail.com>
2025-10-22 21:02:50 -0400

    Implement two pointer approach discussion solution in arrays_and_strings/container_with_most_water.cpp

--- arrays_and_strings/container_with_most_water.cpp
+++ arrays_and_strings/container_with_most_water.cpp
@@ -1,25 +1,48 @@
 #include <catch2/catch_test_macros.hpp"
 
+#include <algorithm>
 #include <vector>
 
-static int maxAreaFA(const std::vector<int>& height)
+static int maxAreaDS2(const std::vector<int>& height)
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/2963/
 
-    
+    int max_water {};
+
+    int left {};
+    int right {static_cast<int>(std::ssize(height)) - 1};
+
+    while (left < right)
+    {
+        const int width {right - left};
+
+        max_water =
+            std::max(max_water, width * std::min(height[left], height[right]));
+
+        if (height[left] <= height[right])
+        {
+            ++left;
+        }
+        else
+        {
+            --right;
+        }
+    }
+
+    return max_water;
 }
 
 TEST_CASE("Example 1", "[maxArea]")
 {
     const std::vector<int> height {1, 8, 6, 2, 5, 4, 8, 3, 7};
 
-    REQUIRE(49 == maxAreaFA(height));
+    REQUIRE(49 == maxAreaDS2(height));
 }
 
 TEST_CASE("Example 2", "[maxArea]")
 {
     const std::vector<int> height {1, 1};
 
-    REQUIRE(1 == maxAreaFA(height));
+    REQUIRE(1 == maxAreaDS2(height));
 }
