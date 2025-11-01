c1ff8fb63a19568ef95e67f490f3a1acc6847193
Jeffry Lew <jeffryklew@gmail.com>
2025-10-31 22:27:29 -0400

    Implement first attempt solution in arrays_and_strings/roman_to_integer.cpp

--- arrays_and_strings/roman_to_integer.cpp
+++ arrays_and_strings/roman_to_integer.cpp
@@ -1,13 +1,48 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
 
 static int romanToIntFA(std::string s)
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/2965/
 
-    //! @todo
+    int              roman_numeral_to_num {};
+    std::string_view roman_numerals {s};
+
+    const std::vector<std::pair<std::string_view, int>> symbols_and_values {
+        {"M", 1000},
+        {"CM", 900},
+        {"D", 500},
+        {"CD", 400},
+        {"C", 100},
+        {"XC", 90},
+        {"L", 50},
+        {"XL", 40},
+        {"X", 10},
+        {"IX", 9},
+        {"V", 5},
+        {"IV", 4},
+        {"I", 1}};
+
+    for (const auto& [roman_numeral, roman_numeral_value] : symbols_and_values)
+    {
+        while (roman_numerals.starts_with(roman_numeral))
+        {
+            roman_numeral_to_num += roman_numeral_value;
+            roman_numerals.remove_prefix(roman_numeral.size());
+        }
+
+        if (roman_numerals.empty())
+        {
+            break;
+        }
+    }
+
+    return roman_numeral_to_num;
 }
 
 TEST_CASE("Example 1", "[romanToInt]")
