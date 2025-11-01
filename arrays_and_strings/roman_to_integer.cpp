7c2c2e722e81e6e74a629a96ad2de4178b14185d
Jeffry Lew <jeffryklew@gmail.com>
2025-11-01 11:40:00 -0400

    Implement left-to-right pass discussion solution in arrays_and_strings/roman_to_integer.cpp

--- arrays_and_strings/roman_to_integer.cpp
+++ arrays_and_strings/roman_to_integer.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -53,17 +54,70 @@ static int romanToIntFA(std::string s)
     return roman_numeral_to_num;
 }
 
+static int romanToIntDS1(std::string s)
+{
+    //! @details https://leetcode.com/problems/roman-to-integer/editorial/
+    //!
+    //!          Time complexity O(1). There is a finite set of Roman numerals
+    //!          with a maximum possible value of 3999 (MMMCMXCIX). If there
+    //!          were an arbitrary number of symbols then the time complexity
+    //!          would be proportional to the length of the input s, i.e. O(N)
+    //!          where N = s.size().
+    //!          Space complexity O(1) since a constant number of single-value
+    //!          variables are used.
+
+    int roman_numeral_to_num {};
+
+    const std::unordered_map<char, int> symbols_to_values {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}};
+
+    const auto s_size = static_cast<int>(std::ssize(s));
+    int        idx {};
+
+    while (idx < s_size)
+    {
+        const char curr_symbol {s[idx]};
+        const int  curr_value {symbols_to_values.at(curr_symbol)};
+
+        //! Check if a next value exists
+        if (idx + 1 < s_size)
+        {
+            const char next_symbol {s[idx + 1]};
+            const int  next_value {symbols_to_values.at(next_symbol)};
+
+            if (curr_value < next_value)
+            {
+                //! The current and next symbols satisfy the subtractive case
+                roman_numeral_to_num += next_value - curr_value;
+                idx += 2;
+                continue;
+            }
+        }
+
+        //! The current symbol and next symbol (if it exists)
+        //! do not satisfy the subtractive case
+        roman_numeral_to_num += curr_value;
+        ++idx;
+    }
+
+    return roman_numeral_to_num;
+}
+
 TEST_CASE("Example 1", "[romanToInt]")
 {
     REQUIRE(3 == romanToIntFA("III"));
+    REQUIRE(3 == romanToIntDS1("III"));
 }
 
 TEST_CASE("Example 2", "[romanToInt]")
 {
     REQUIRE(58 == romanToIntFA("LVIII"));
+    REQUIRE(58 == romanToIntDS1("LVIII"));
 }
 
 TEST_CASE("Example 3", "[romanToInt]")
 {
-    REQUIRE(1994 == romainToIntFA("MCMXCIV"));
+    REQUIRE(1994 == romanToIntFA("MCMXCIV"));
+    REQUIRE(1994 == romanToIntDS1("MCMXCIV"));
 }
