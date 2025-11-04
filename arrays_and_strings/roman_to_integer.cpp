de99a67c3276f688979fa09ebebd9b04da5a26f7
Jeffry Lew <jeffryklew@gmail.com>
2025-11-03 21:17:20 -0500

    Implement left-to-right pass improved discussion solution in arrays_and_strings/roman_to_integer.cpp

--- arrays_and_strings/roman_to_integer.cpp
+++ arrays_and_strings/roman_to_integer.cpp
@@ -108,7 +108,48 @@ static int romanToIntDS2(std::string s)
 {
     //! @details https://leetcode.com/problems/roman-to-integer/editorial/
 
-    //! @todo
+    const std::unordered_map<std::string, int> symbols_to_values {
+        {"I", 1},
+        {"V", 5},
+        {"X", 10},
+        {"L", 50},
+        {"C", 100},
+        {"D", 500},
+        {"M", 1000},
+        {"IV", 4},
+        {"IX", 9},
+        {"XL", 40},
+        {"XC", 90},
+        {"CD", 400},
+        {"CM", 900}};
+
+    const auto s_size = static_cast<int>(std::ssize(s));
+
+    int roman_numeral_to_num {};
+    int idx {};
+
+    while (idx < s_size)
+    {
+        if (idx < s_size - 1)
+        {
+            const auto double_symbol = s.substr(idx, 2);
+
+            //! Check if this is the size() - 2 symbol case
+            if (symbols_to_values.contains(double_symbol))
+            {
+                roman_numeral_to_num += symbols_to_values.at(double_symbol);
+                idx += 2;
+                continue;
+            }
+        }
+
+        //! Otherwise, this is the size() - 1 symbol case
+        const auto single_symbol = s.substr(idx, 1);
+        roman_numeral_to_num += symbols_to_values.at(single_symbol);
+        ++idx;
+    }
+
+    return roman_numeral_to_num;
 }
 
 TEST_CASE("Example 1", "[romanToInt]")
