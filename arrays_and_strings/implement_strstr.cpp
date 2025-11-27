bc29868a0b7e1b31b0f7b179617effc426ac82f3
Jeffry Lew <jeffryklew@gmail.com>
2025-11-27 10:05:49 -0500

    Implement first attempt solution in arrays_and_strings/implement_strstr.cpp

---
+++ arrays_and_strings/implement_strstr.cpp
@@ -0,0 +1,59 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <string>
+
+static int strStrFA(std::string haystack, std::string needle)
+{
+    //! @details https://leetcode.com/explore/interview/card/amazon/76
+    //!          /array-and-strings/2968/
+
+    const auto haystack_size = static_cast<int>(std::ssize(haystack));
+    const auto needle_size   = static_cast<int>(std::ssize(needle));
+
+    for (int haystack_pos = 0; haystack_pos < haystack_size; ++haystack_pos)
+    {
+        if (haystack_size - haystack_pos < needle_size)
+        {
+            return -1;
+        }
+
+        if (haystack[haystack_pos] != needle.front())
+        {
+            continue;
+        }
+
+        int pos_offset {1};
+        while (pos_offset < needle_size)
+        {
+            if (haystack[haystack_pos + pos_offset] != needle[pos_offset])
+            {
+                break;
+            }
+
+            ++pos_offset;
+        }
+
+        if (pos_offset == needle_size)
+        {
+            return haystack_pos;
+        }
+    }
+
+    return -1;
+}
+
+TEST_CASE("Example 1", "[strStr]")
+{
+    const std::string haystack {"sadbutsad"};
+    const std::string needle {"sad"};
+
+    REQUIRE(0 == strStrFA(haystack, needle));
+}
+
+TEST_CASE("Example 2", "[strStr]")
+{
+    const std::string haystack {"leetcode"};
+    const std::string needle {"leeto"};
+
+    REQUIRE(-1 == strStrFA(haystack, needle));
+}
