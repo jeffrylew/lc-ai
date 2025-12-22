be669e3d766dca4512740ea501cbce9af66239e7
Jeffry Lew <jeffryklew@gmail.com>
2025-12-21 20:55:50 -0800

    WIP Continue first attempt solution in arrays_and_strings/compare_version_numbers.cpp

--- arrays_and_strings/compare_version_numbers.cpp
+++ arrays_and_strings/compare_version_numbers.cpp
@@ -13,7 +13,47 @@ static int compareVersionFA(std::string version1, std::string version2)
     int v1_idx {};
     int v2_idx {};
 
+    int v1_dot_count {};
+    int v2_dot_count {};
+
     while (v1_idx < v1_size && v2_idx < v2_size)
+    {
+        if (version1[v1_idx] == '.')
+        {
+            ++v1_dot_count;
+        }
+
+        if (version2[v2_idx] == '.')
+        {
+            ++v2_dot_count;
+        }
+
+        sub_v1_val = 10 * sub_v1_val + static_cast<int>(version1[v1_idx] - '0');
+        sub_v2_val = 10 * sub_v2_val + static_cast<int>(version2[v2_idx] - '0');
+
+        if (v1_dot_count > 0 && v1_dot_count == v2_dot_count)
+        {
+            if (sub_v1_val < sub_v2_val)
+            {
+                return -1;
+            }
+            else if (sub_v1_val > sub_v2_val)
+            {
+                return 1;
+            }
+
+            sub_v1_val = 0;
+            sub_v2_val = 0;
+        }
+
+        ++v1_idx;
+        ++v2_idx;
+    }
+
+    sub_v1_val = 0;
+    sub_v2_val = 0;
+
+    while (v1_idx < v1_size)
     {
         if (version1[v1_idx] == '.')
         {
@@ -21,14 +61,33 @@ static int compareVersionFA(std::string version1, std::string version2)
             ++v1_idx;
         }
 
+        sub_v1_val = 10 * sub_v1_val + static_cast<int>(version1[v1_idx] - '0');
+        if (sub_v1_val > 0)
+        {
+            return 1;
+        }
+
+        ++v1_idx;
+    }
+
+    while (v2_idx < v2_size)
+    {
         if (version2[v2_idx] == '.')
         {
             sub_v2_val = 0;
             ++v2_idx;
         }
 
-        //! @todo
+        sub_v2_val = 10 * sub_v2_val + static_cast<int>(version2[v2_idx] - '0');
+        if (sub_v2_val > 0)
+        {
+            return -1;
+        }
+
+        ++v2_idx;
     }
+
+    return 0;
 }
 
 TEST_CASE("Example 1", "[compareVersion]")
