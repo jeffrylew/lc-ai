29e496fecec963c3bbaf82c22c1d59ba11e9a8f9
Jeffry Lew <jeffryklew@gmail.com>
2025-12-31 19:19:03 -0800

    Implement rotate groups of four cells discussion solution in arrays_and_strings/rotate_image.cpp

--- arrays_and_strings/rotate_image.cpp
+++ arrays_and_strings/rotate_image.cpp
@@ -2,32 +2,47 @@
 
 #include <vector>
 
-static void rotateFA(const std::vector<std::vector<int>>& matrix)
+static void rotateDS1(std::vector<std::vector<int>>& matrix)
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/2969/
 
-    //! @todo
+    const auto n = static_cast<int>(std::ssize(matrix));
+
+    for (int i = 0; i < (n + 1) / 2; ++i)
+    {
+        for (int j = 0; j < n / 2; ++j)
+        {
+            const int temp {matrix[n - 1 - j][i]};
+
+            matrix[n - 1 - j][i]         = matrix[n - 1 - i][n - j - 1];
+            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i]         = matrix[i][j];
+            matrix[i][j]                 = temp;
+        }
+    }
 }
 
 TEST_CASE("Example 1", "[rotate]")
 {
     const std::vector<std::vector<int>> matrix {
         {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    auto matrix_copy = matrix;
 
     const std::vector<std::vector<int>> expected_output {
         {7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
 
-    REQUIRE(expected_output == rotateFA(matrix));
+    REQUIRE(expected_output == rotateDS1(matrix_copy));
 }
 
 TEST_CASE("Example 2", "[rotate]")
 {
     const std::vector<std::vector<int>> matrix {
         {5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    auto matrix_copy = matrix;
 
     const std::vector<std::vector<int>> expected_output {
         {15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}};
 
-    REQUIRE(expected_output == rotateFA(matrix));
+    REQUIRE(expected_output == rotateDS1(matrix_copy));
 }
