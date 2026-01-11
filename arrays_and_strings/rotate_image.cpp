8b806cdb81c499abc2822c8f04146324beb5128f
Jeffry Lew <jeffryklew@gmail.com>
2026-01-11 13:22:06 -0500

    Add example comments to arrays_and_strings/rotate_image.cpp

--- arrays_and_strings/rotate_image.cpp
+++ arrays_and_strings/rotate_image.cpp
@@ -45,11 +45,11 @@ static void reflect(std::vector<std::vector<int>>& matrix)
 {
     const auto matrix_size = static_cast<int>(std::ssize(matrix));
 
-    for (int i = 0; i < matrix_size; ++i)
+    for (int row = 0; row < matrix_size; ++row)
     {
-        for (int j = 0; j < matrix_size / 2; ++j)
+        for (int col = 0; col < matrix_size / 2; ++col)
         {
-            std::swap(matrix[i][j], matrix[i][matrix_size - j - 1]);
+            std::swap(matrix[row][col], matrix[row][matrix_size - col - 1]);
         }
     }
 }
@@ -70,6 +70,9 @@ static void rotateDS2(std::vector<std::vector<int>>& matrix)
 
 TEST_CASE("Example 1", "[rotate]")
 {
+    // 1 2 3    7 4 1
+    // 4 5 6 -> 8 5 2
+    // 7 8 9    9 6 3
     const std::vector<std::vector<int>> matrix {
         {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     auto matrix_copy = matrix;
@@ -77,12 +80,40 @@ TEST_CASE("Example 1", "[rotate]")
     const std::vector<std::vector<int>> expected_output {
         {7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
 
+    // for i = 0 to (3 + 1) / 2 = 2
+    //   for j = 0 to 3 / 2 = 1
+    //     - i = 0; j = 0
+    //       temp     = mc[3 - 1 - 0][0]         = mc[2][0] // temp     = 7
+    //       mc[2][0] = mc[3 - 1 - 0][3 - 0 - 1] = mc[2][2] // mc[2][0] = 9
+    //       mc[2][2] = mc[0][3 - 1 - 0]         = mc[0][2] // mc[2][2] = 3
+    //       mc[0][2] = mc[0][0]                            // mc[0][2] = 1
+    //       mc[0][0] = temp                                // mc[0][0] = 7
+    //     - i = 1; j = 0
+    //       temp     = mc[3 - 1 - 0][1]         = mc[2][1] // temp     = 8
+    //       mc[2][1] = mc[3 - 1 - 1][3 - 0 - 1] = mc[1][2] // mc[2][1] = 6
+    //       mc[1][2] = mc[0][3 - 1 - 1]         = mc[0][1] // mc[1][2] = 2
+    //       mc[0][1] = mc[1][0]                            // mc[0][1] = 4
+    //       mc[1][0] = temp                                // mc[1][0] = 8
     REQUIRE(expected_output == rotateDS1(matrix_copy));
+
+    //       Transpose  Reflect/Reverse
+    // 1 2 3    1 4 7    7 4 1
+    // 4 5 6 -> 2 5 8 -> 8 5 2
+    // 7 8 9    3 6 9    9 6 3
+
+    //         Reflect  Transpose
+    // 1 2 3    3 2 1    3 6 9
+    // 4 5 6 -> 6 5 4 -> 2 5 8     INCORRECT, must transpose before reflect
+    // 7 8 9    9 8 7    1 4 7
     REQUIRE(expected_output == rotateDS2(matrix_copy));
 }
 
 TEST_CASE("Example 2", "[rotate]")
 {
+    // 05 01 09 11    15 13 02 05
+    // 02 04 08 10 -> 14 03 04 01
+    // 13 03 06 07    12 06 08 09
+    // 15 14 12 16    16 07 10 11
     const std::vector<std::vector<int>> matrix {
         {5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
     auto matrix_copy = matrix;
