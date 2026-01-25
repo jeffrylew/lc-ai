da14deb160b27b3595790b86956860bbd755765b
Jeffry Lew <jeffryklew@gmail.com>
2026-01-24 21:32:22 -0500

    Implement sorting discussion solution in arrays_and_strings/missing_number.cpp

--- arrays_and_strings/missing_number.cpp
+++ arrays_and_strings/missing_number.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
 #include <numeric>
 #include <vector>
 
@@ -17,11 +18,44 @@ static int missingNumberFA(const std::vector<int>& nums)
     return expected_sum - std::accumulate(nums.begin(), nums.end(), 0);
 }
 
+static int missingNumberDS1(const std::vector<int>& nums)
+{
+    auto sorted_nums = nums;
+    std::ranges::sort(sorted_nums);
+
+    const auto nums_size = static_cast<int>(std::ssize(sorted_nums));
+
+    if (sorted_nums.back() != nums_size)
+    {
+        //! Ensure that n is at the last index
+        return nums_size;
+    }
+    else if (sorted_nums.front() != 0)
+    {
+        //! Ensure that 0 is at the first index
+        return 0;
+    }
+
+    for (int idx = 1; idx < nums_size; ++idx)
+    {
+        const int expected_num {sorted_nums[idx - 1] + 1};
+
+        if (sorted_nums[idx] != expected_num)
+        {
+            return expected_num;
+        }
+    }
+
+    //! Vector was not missing any numbers
+    return -1;
+}
+
 TEST_CASE("Example 1", "[missingNumber]")
 {
     const std::vector<int> nums {3, 0, 1};
 
     REQUIRE(2 == missingNumberFA(nums));
+    REQUIRE(2 == missingNumberDS1(nums));
 }
 
 TEST_CASE("Example 2", "[missingNumber]")
@@ -29,6 +63,7 @@ TEST_CASE("Example 2", "[missingNumber]")
     const std::vector<int> nums {0, 1};
 
     REQUIRE(2 == missingNumberFA(nums));
+    REQUIRE(2 == missingNumberDS1(nums));
 }
 
 TEST_CASE("Example 3", "[missingNumber]")
@@ -36,4 +71,5 @@ TEST_CASE("Example 3", "[missingNumber]")
     const std::vector<int> nums {9, 6, 4, 2, 3, 5, 7, 0, 1};
 
     REQUIRE(8 == missingNumberFA(nums));
+    REQUIRE(8 == missingNumberDS1(nums));
 }
