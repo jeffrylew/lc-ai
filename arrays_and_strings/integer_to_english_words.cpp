6f91cc5641a3493353d412341c6b26fe39491063
Jeffry Lew <jeffryklew@gmail.com>
2026-02-02 22:34:31 -0500

    WIP Partially implement recursive approach discussion solution in arrays_and_strings/integer_to_english_words.cpp

--- arrays_and_strings/integer_to_english_words.cpp
+++ arrays_and_strings/integer_to_english_words.cpp
@@ -1,13 +1,76 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <string>
+#include <vector>
 
-static std::string numberToWordsFA(int num)
+static std::string convertToWords(int num)
+{
+    if (num < 10)
+    {
+        static const std::vector<std::string> belowTen {
+            "",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine"};
+        return belowTen[num];
+    }
+
+    if (num < 20)
+    {
+        static const std::vector<std::string> belowTwenty {
+            "Ten",
+            "Eleven",
+            "Twelve",
+            "Thirteen",
+            "Fourteen",
+            "Fifteen",
+            "Sixteen",
+            "Seventeen",
+            "Eighteen",
+            "Nineteen"};
+        return belowTwenty[num - 10];
+    }
+
+    if (num < 100)
+    {
+        static const std::vector<std::string> belowHundred {
+            "",
+            "Ten",
+            "Twenty",
+            "Thirty",
+            "Forty",
+            "Fifty",
+            "Sixty",
+            "Seventy",
+            "Eighty",
+            "Ninety"};
+        return belowHundred[num / 10]
+            + (num % 10 ? " " + convertToWords(num % 10) : "");
+    }
+
+    if (num < 1000)
+    {
+        //! @todo
+    }
+}
+
+static std::string numberToWordsDS1(int num)
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/481/
 
-    //! @todo
+    if (num == 0)
+    {
+        return "Zero";
+    }
+
+    return convertToWords(num);
 }
 
 TEST_CASE("Example 1", "[numberToWords]")
@@ -15,7 +78,7 @@ TEST_CASE("Example 1", "[numberToWords]")
     constexpr const char* expected_output {
         "One Hundred Twenty Three"};
 
-    REQUIRE(expected_output == numberToWordsFA(123));
+    REQUIRE(expected_output == numberToWordsDS1(123));
 }
 
 TEST_CASE("Example 2", "[numberToWords]")
@@ -23,7 +86,7 @@ TEST_CASE("Example 2", "[numberToWords]")
     constexpr const char* expected_output {
         "Twelve Thousand Three Hundred Forty Five"};
 
-    REQUIRE(expected_output == numberToWordsFA(12345));
+    REQUIRE(expected_output == numberToWordsDS1(12345));
 }
 
 TEST_CASE("Example 3", "[numberToWords]")
@@ -32,5 +95,5 @@ TEST_CASE("Example 3", "[numberToWords]")
         "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
     };
 
-    REQUIRE(expected_output == numberToWordsFA(1234567));
+    REQUIRE(expected_output == numberToWordsDS1(1234567));
 }
