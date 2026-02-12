129dde9e951dac140d07060d6b6438f890ac6573
Jeffry Lew <jeffryklew@gmail.com>
2026-02-11 19:20:55 -0500

    Actually implement first attempt solution in arrays_and_strings/valid_parentheses.cpp

--- arrays_and_strings/valid_parentheses.cpp
+++ arrays_and_strings/valid_parentheses.cpp
@@ -1,13 +1,41 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <stack>
 #include <string>
+#include <unordered_map>
 
 static bool isValidFA(std::string s)
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/2972/
 
-    
+    const std::unordered_map<char, char> matching_brackets {
+        {'(', ')'}, {'{', '}'}, {'[', ']'}};
+
+    std::stack<char> left_brackets;
+    for (const char curr_bracket : s)
+    {
+        if (matching_brackets.contains(curr_bracket))
+        {
+            left_brackets.push(curr_bracket);
+            continue;
+        }
+
+        if (left_brackets.empty())
+        {
+            return false;
+        }
+
+        const char prev_left_bracket {left_brackets.top()};
+        left_brackets.pop();
+
+        if (matching_brackets.at(prev_left_bracket) != curr_bracket)
+        {
+            return false;
+        }
+    }
+
+    return left_brackets.empty();
 }
 
 TEST_CASE("Example 1", "[isValid]")
