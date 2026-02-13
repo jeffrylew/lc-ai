1036d3603faa2e9c28bad65a7a4b9dd8677c1d76
Jeffry Lew <jeffryklew@gmail.com>
2026-02-13 04:31:44 -0500

    Implement stacks discussion solution in arrays_and_strings/valid_parentheses.cpp

--- arrays_and_strings/valid_parentheses.cpp
+++ arrays_and_strings/valid_parentheses.cpp
@@ -42,27 +42,69 @@ static bool isValidFA(std::string s)
     return left_brackets.empty();
 }
 
+static bool isValidDS(std::string s)
+{
+    //! @details https://leetcode.com/problems/valid-parentheses/editorial/
+
+    const std::unordered_map<char, char> matching_brackets {
+        {')', '('}, {'}', '{'}, {']', '['}};
+
+    std::stack<char> opening_brackets;
+    for (const char curr_bracket : s)
+    {
+        //! Opening brackets
+        if (!matching_brackets.contains(curr_bracket))
+        {
+            opening_brackets.push(curr_bracket);
+            continue;
+        }
+
+        //! Closing brackets: Get the top element of the stack
+        //! If stack is empty, set a dummy value '#'
+        const char prev_opening_bracket {
+            opening_brackets.empty() ? '#' : opening_brackets.top()};
+        if (!opening_brackets.empty())
+        {
+            opening_brackets.pop();
+        }
+
+        //! If the mapping for this current bracket doesn't
+        //! match the previous opening bracket, return false
+        if (prev_opening_bracket != matching_brackets.at(curr_bracket))
+        {
+            return false;
+        }
+    }
+
+    return opening_brackets.empty();
+}
+
 TEST_CASE("Example 1", "[isValid]")
 {
     REQUIRE(isValidFA("()"));
+    REQUIRE(isValidDS("()"));
 }
 
 TEST_CASE("Example 2", "[isValid]")
 {
     REQUIRE(isValidFA("()[]{}"));
+    REQUIRE(isValidDS("()[]{}"));
 }
 
 TEST_CASE("Example 3", "[isValid]")
 {
     REQUIRE(!isValidFA("(]"));
+    REQUIRE(!isValidDS("(]"));
 }
 
 TEST_CASE("Example 4", "[isValid]")
 {
     REQUIRE(isValidFA("([])"));
+    REQUIRE(isValidDS("([])"));
 }
 
 TEST_CASE("Example 5", "[isValid]")
 {
     REQUIRE(!isValidFA("([)]"));
+    REQUIRE(!isValidDS("([)]"));
 }
