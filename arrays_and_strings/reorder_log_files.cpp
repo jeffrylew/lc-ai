b9e1cc5e801a7718f42ace3f86f6a36cdd405096
Jeffry Lew <jeffryklew@gmail.com>
2026-02-23 22:08:18 -0500

    Implement first attempt solution in arrays_and_strings/reorder_log_files.cpp

--- arrays_and_strings/reorder_log_files.cpp
+++ arrays_and_strings/reorder_log_files.cpp
@@ -1,6 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
+#include <cctype>
+#include <ranges>
 #include <string>
+#include <string_view>
 #include <vector>
 
 static std::vector<std::string> reorderLogFilesFA(
@@ -8,6 +12,68 @@ static std::vector<std::string> reorderLogFilesFA(
 {
     //! @details https://leetcode.com/explore/interview/card/amazon/76
     //!          /array-and-strings/2974/
+
+    struct Letter_log
+    {
+        std::string_view              log_view;
+        std::string_view              identifier;
+        std::vector<std::string_view> words;
+
+        constexpr Letter_log(std::string_view log_line)
+            : log_view {log_line}
+        {
+            const auto first_space_pos = log_view.find_first_of(' ');
+            identifier                 = log_view.substr(0, first_space_pos);
+
+            auto range_of_word_views =
+                log_view.substr(first_space_pos + 1U) | std::views::split(' ');
+            for (const auto& word_view : range_of_word_views)
+            {
+                words.emplace_back(word_view.begin(), word_view.end());
+            }
+        }
+    };
+
+    std::vector<Letter_log>       letter_logs;
+    std::vector<std::string_view> digit_logs;
+
+    for (const auto& log : logs)
+    {
+        const std::string_view log_view {log};
+
+        if (std::isdigit(static_cast<unsigned char>(log.back())) != 0)
+        {
+            digit_logs.push_back(log_view);
+            continue;
+        }
+
+        letter_logs.emplace_back(log_view);
+    }
+
+    std::ranges::sort(letter_logs,
+                      [](const Letter_log& lhs, const Letter_log& rhs) {
+                          if (lhs.words == rhs.words)
+                          {
+                              return lhs.identifier < rhs.identifier;
+                          }
+    
+                          return lhs.words < rhs.words;
+                      });
+
+    std::vector<std::string> final_order;
+    final_order.reserve(logs.size());
+
+    for (const auto& letter_log : letter_logs)
+    {
+        final_order.emplace_back(std::string {letter_log.log_view});
+    }
+
+    for (const auto& digit_log : digit_logs)
+    {
+        final_order.emplace_back(std::string {digit_log});
+    }
+
+    return final_order;
 }
 
 TEST_CASE("Example 1", "[reorderLogFiles]")
