799647ffb7dd0db67e01410b7395b72a48ec0e2a
Jeffry Lew <jeffryklew@gmail.com>
2026-03-03 21:55:26 -0500

    WIP Start first attempt solution in trees_and_graphs/cut_off_trees_for_golf_event.cpp

--- trees_and_graphs/cut_off_trees_for_golf_event.cpp
+++ trees_and_graphs/cut_off_trees_for_golf_event.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <queue>
+#include <utility>
 #include <vector>
 
 static int cutOffTreeFA(const std::vector<std::vector<int>>& forest)
@@ -7,7 +9,40 @@ static int cutOffTreeFA(const std::vector<std::vector<int>>& forest)
     //! @details https://leetcode.com/explore/interview/card/amazon/78
     //!          /trees-and-graphs/2986/
 
+    const auto num_rows    = static_cast<int>(std::ssize(forest));
+    const auto num_cols    = static_cast<int>(std::ssize(forest.at(0)))
+    auto       forest_copy = forest;
 
+    int min_steps {};
+
+    const std::vector<std::pair<int, int>> directions {
+        {-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+
+    constexpr auto is_pos_valid = [=](int row, int col) -> bool {
+        return row >= 0 && row < num_rows && col >= 0 && col < num_cols;
+    };
+
+    std::queue<std::pair<int, int>> pos_queue;
+    pos_queue.emplace(0, 0);
+
+    while (!pos_queue.empty())
+    {
+        const auto pos_queue_size = static_cast<int>(std::ssize(pos_queue));
+
+        for (int num_cells = 0; num_cells < pos_queue_size; ++num_cells)
+        {
+            const auto [curr_row, curr_col] = pos_queue.front();
+            pos_queue.pop();
+
+            auto& cell_value = forest_copy[curr_row][curr_col];
+            if (cell_value == 0)
+            {
+                continue;
+            }
+
+            //! @todo
+        }
+    }
 }
 
 TEST_CASE("Example 1", "[cutOffTree]")
