a843f9127ec7ec88e6ee888eb6f1729038425699
Jeffry Lew <jeffryklew@gmail.com>
2026-03-20 21:20:49 -0400

    Start first attempt solution in trees_and_graphs/word_ladder_ii.cpp

--- trees_and_graphs/word_ladder_ii.cpp
+++ trees_and_graphs/word_ladder_ii.cpp
@@ -1,6 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <queue>
 #include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 static std::vector<std::vector<std::string>> findLaddersFA(
@@ -8,7 +12,43 @@ static std::vector<std::vector<std::string>> findLaddersFA(
     std::string                     endWord,
     const std::vector<std::string>& wordList)
 {
+    //! @details https://leetcode.com/explore/interview/card/amazon/78
+    //!          /trees-and-graphs/483/
 
+    //! Map of <word neighbor, word>
+    std::unordered_map<std::string, std::vector<std::string_view>>
+        word_neighbor_map;
+
+    for (const auto& word : wordList)
+    {
+        const auto word_size = static_cast<int>(std::ssize(word));
+
+        for (int word_idx = 0; word_idx < word_size; ++word_idx)
+        {
+            std::string neighbor {word};
+            neighbor[word_idx] = '#';
+            word_neighbor_map[std::move(neighbor)].emplace_back(
+                std::string_view {word});
+        }
+    }
+
+    std::vector<std::vector<std::string>> shortest_sequences;
+
+    const auto begin_word_size = static_cast<int>(std::ssize(beginWord));
+    for (int begin_idx = 0; begin_idx < begin_word_size; ++begin_idx)
+    {
+        std::string begin_word_neighbor {beginWord};
+        begin_word_neighbor[begin_idx] = '#';
+
+        if (!word_neighbor_map.contains(begin_word_neighbor))
+        {
+            continue;
+        }
+
+        //! @todo
+    }
+
+    return shortest_sequences;
 }
 
 TEST_CASE("Example 1", "[findLadders]")
