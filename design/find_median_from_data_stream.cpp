8ca5630b867ee5006e274f3e5a4c9e0c0105f305
Jeffry Lew <jeffryklew@gmail.com>
2026-04-12 09:57:55 -0400

    Handle negative numbers in first attempt solution in design/find_median_from_data_stream.cpp

--- design/find_median_from_data_stream.cpp
+++ design/find_median_from_data_stream.cpp
@@ -11,11 +11,24 @@ class MedianFinderFA
 public:
     void addNum(int num)
     {
-        min_heap.push(num);
+        if (num >= 0)
+        {
+            min_heap.push(num);
+        }
+        else
+        {
+            max_heap.push(num);
+        }
+
         if (min_heap.size() > max_heap.size())
         {
             max_heap.push(min_heap.top());
             min_heap.pop();
+        }
+        else if (max_heap.size() > min_heap.size())
+        {
+            min_heap.push(max_heap.top());
+            max_heap.pop();
         }
     }
 
@@ -24,6 +37,11 @@ public:
         if (max_heap.size() > min_heap.size())
         {
             return static_cast<double>(max_heap.top());
+        }
+
+        if (min_heap.size() > max_heap.size())
+        {
+            return static_cast<double>(min_heap.top());
         }
 
         return static_cast<double>(min_heap.top() + max_heap.top()) / 2.0;
@@ -36,19 +54,45 @@ private:
 
 TEST_CASE("Example 1", "[MedianFinder]")
 {
-    MedianFinderFA median_finder;
+    MedianFinderFA median_finder_fa;
 
     // min: [1] -> []
-    // max: []  -> [1], retrieve from max for first median b/c sizes not equal
+    // max: []  -> [1], retrieve from max.top()
     // min: [2] -> [2]
     // max: [1] -> [1], average min.top() and max.top()
     // min: [2, 3] -> [3]
     // max: [1]    -> [2, 1], retrieve from max.top()
     // min: [3, 4] -> [3, 4]
     // max: [2, 1] -> [2, 1], average min.top() and max.top()
-    median_finder.addNum(1);                  // arr = [1]
-    median_finder.addNum(2);                  // arr = [1, 2]
-    CHECK(1.5 == median_finder.findMedian()); // return 1.5 = (1 + 2) / 2
-    median_finder.addNum(3);                  // arr = [1, 2, 3]
-    CHECK(2.0 == median_finder.findMedian()); // return 2.0
+    median_finder_fa.addNum(1);                  // arr = [1]
+    median_finder_fa.addNum(2);                  // arr = [1, 2]
+    CHECK(1.5 == median_finder_fa.findMedian()); // return 1.5 = (1 + 2) / 2
+    median_finder_fa.addNum(3);                  // arr = [1, 2, 3]
+    CHECK(2.0 == median_finder_fa.findMedian()); // return 2.0
+}
+
+TEST_CASE("Example 2", "[MedianFinder]")
+{
+    MedianFinderFA median_finder_fa;
+
+    // min: []   -> [-1], retrieve from min.top()
+    // max: [-1] -> []
+    // min: [-1] -> [-1], average min.top() and max.top()
+    // max: [-2] -> [-2]
+    // min: [-1]     -> [-2, -1], retrieve from min.top()
+    // max: [-2, -3] -> [-3]
+    // min: [-2, -1] -> [-2, -1], average min.top() and max.top()
+    // max: [-3, -4] -> [-3, -4]
+    // min: [-2, -1]     -> [-3, -2, -1], retrieve from min.top()
+    // max: [-3, -4, -5] -> [-4, -5]
+    median_finder_fa.addNum(-1);                  // arr = [-1]
+    CHECK(-1.0 == median_finder_fa.findMedian()); // return -1.0
+    median_finder_fa.addNum(-2);                  // arr = [-1, -2]
+    CHECK(-1.5 == median_finder_fa.findMedian()); // return -1.5
+    median_finder_fa.addNum(-3);                  // arr = [-1, -2, -3]
+    CHECK(-2.0 == median_finder_fa.findMedian()); // return -2.0
+    median_finder_fa.addNum(-4);                  // arr = [-1, -2, -3, -4]
+    CHECK(-2.5 == median_finder_fa.findMedian()); // return -2.5
+    median_finder_fa.addNum(-5);                  // arr = [-1, -2, -3, -4, -5]
+    CHECK(-3.0 == median_finder_fa.findMedian()); // return -3.0
 }
